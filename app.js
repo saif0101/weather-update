33fd53ce2f25851d65de63733464a9ae972c69fe
saif0101 <saif13@cse.pstu.ac.bd>
2022-06-24 12:35:01 +0600

    add some info

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ document.getElementById('cityName').value ='';
 
 function display(id) {
     var celsius = Math.round(parseFloat(id.main.temp)-273.15);
+    var celsius1 = Math.round(parseFloat(id.main.feels_like)-273.15);
 
     document.getElementById('temp-city').innerHTML = id.name;
     document.getElementById('temp').innerHTML = celsius;
     document.getElementById('condition').innerHTML = id.weather[0].main;
     document.getElementById('condition-icon').src = "http://openweathermap.org/img/w/"+id.weather[0].icon+".png";
+
+    document.getElementById('feels').innerHTML = celsius1;
+    document.getElementById('humidity').innerHTML = id.main.humidity;
+    document.getElementById('pressure').innerHTML = id.main.pressure;
+    document.getElementById('speed').innerHTML = id.wind.speed;
     console.log(id);
 }
 
