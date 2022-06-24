d09096e63a9f0324b4c49dd286850d23db7002ff
saif0101 <saif13@cse.pstu.ac.bd>
2022-06-24 12:42:36 +0600

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var apiKey ='094ce1db02356bf72aa305f5dd4dd5b3';
 function citySearch () {
     const city = document.getElementById('cityName').value;
-    const queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid='+ apiKey;
+    const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid='+ apiKey;
 
 fetch(queryURL)
 .then(res => res.json())
@@ -18,13 +18,13 @@ function display(id) {
     document.getElementById('temp-city').innerHTML = id.name;
     document.getElementById('temp').innerHTML = celsius;
     document.getElementById('condition').innerHTML = id.weather[0].main;
-    document.getElementById('condition-icon').src = "http://openweathermap.org/img/w/"+id.weather[0].icon+".png";
+    document.getElementById('condition-icon').src = "https://openweathermap.org/img/w/"+id.weather[0].icon+".png";
 
     document.getElementById('feels').innerHTML = celsius1;
     document.getElementById('humidity').innerHTML = id.main.humidity;
     document.getElementById('pressure').innerHTML = id.main.pressure;
     document.getElementById('speed').innerHTML = id.wind.speed;
-    console.log(id);
+
 }
 
 
